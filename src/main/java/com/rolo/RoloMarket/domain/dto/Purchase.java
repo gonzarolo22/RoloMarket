9eb6ea1f07fd6c7751b0c73cd6ef7ca1da845f9f
gonza rolo <gonzarolo22@gmail.com>
2023-03-14 15:10:43 -0300

    dto purchase terminado a revisar

--- src/main/java/com/rolo/RoloMarket/domain/dto/Purchase.java
+++ src/main/java/com/rolo/RoloMarket/domain/dto/Purchase.java
@@ -2,10 +2,42 @@ package com.rolo.RoloMarket.domain.dto;
 
 import com.rolo.RoloMarket.persistence.entity.Clientes;
 
+import java.time.LocalDate;
+
 public class Purchase {
     private int idPurchase;
-    private  String buy;
+    private  int idClient;
+    private LocalDate fecha;
+    private String buy;
     private String comentary;
     private String status;
+    private Client client;
 
+    public int getIdPurchase() {
+        return idPurchase;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getBuy() {
+        return buy;
+    }
+
+    public String getComentary() {
+        return comentary;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Client getClient() {
+        return client;
+    }
 }
