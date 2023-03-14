e625696ef9c84a42ababa60c41d6a8edb1759bc7
gonza rolo <gonzarolo22@gmail.com>
2023-03-13 21:15:34 -0300

    completando dtos

---
+++ src/main/java/com/rolo/RoloMarket/domain/dto/Purchase.java
@@ -0,0 +1,11 @@
+package com.rolo.RoloMarket.domain.dto;
+
+import com.rolo.RoloMarket.persistence.entity.Clientes;
+
+public class Purchase {
+    private int idPurchase;
+    private  String buy;
+    private String comentary;
+    private String status;
+
+}
--- src/main/java/com/rolo/RoloMarket/domain/dto/purchase.java
+++
@@ -1,6 +0,0 @@
-package com.rolo.RoloMarket.domain.dto;
-
-public class purchase {
-    private int idPurchase;
-    private  String buy;
-}
