f90769dcd41910284a8134eaaffe2b70df41d34e
gonza rolo <gonzarolo22@gmail.com>
2023-03-16 11:46:11 -0300

    empecé mapper de purchase item

---
+++ src/main/java/com/rolo/RoloMarket/persistence/Mapper/PurchaseItemMapper.java
@@ -0,0 +1,20 @@
+package com.rolo.RoloMarket.persistence.Mapper;
+
+import com.rolo.RoloMarket.domain.dto.PurchaseItem;
+import com.rolo.RoloMarket.persistence.entity.CompraProducto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface PurchaseItemMapper {
+
+    @Mappings({
+            @Mapping(source="id.id_producto", target = "productId"),
+            @Mapping(source = "cantidad", target = "amount"),
+            @Mapping(source = "estado", target = "status")
+    })
+
+    PurchaseItem toPurchaseItem(CompraProducto compraProducto);
+
+}
