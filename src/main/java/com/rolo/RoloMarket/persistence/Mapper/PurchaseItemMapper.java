e68e59450a096788009f3f33c439462af9048498
gonza rolo <gonzarolo22@gmail.com>
2023-03-17 23:43:48 -0300

    .

--- src/main/java/com/rolo/RoloMarket/persistence/Mapper/PurchaseItemMapper.java
+++ src/main/java/com/rolo/RoloMarket/persistence/Mapper/PurchaseItemMapper.java
@@ -2,6 +2,7 @@ package com.rolo.RoloMarket.persistence.Mapper;
 
 import com.rolo.RoloMarket.domain.dto.PurchaseItem;
 import com.rolo.RoloMarket.persistence.entity.CompraProducto;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -14,7 +15,10 @@ public interface PurchaseItemMapper {
             @Mapping(source = "cantidad", target = "amount"),
             @Mapping(source = "estado", target = "status")
     })
-
     PurchaseItem toPurchaseItem(CompraProducto compraProducto);
+
+    @InheritInverseConfiguration
+    @Mapping()
+    CompraProducto toCompraProducto(PurchaseItem purchaseItem);
 
 }
